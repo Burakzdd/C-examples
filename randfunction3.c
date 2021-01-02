d5eab0caf07487d10dc8f243c20611b1c6c93440
Burak Özdemir <75833960+Burakzdd@users.noreply.github.com>
2021-01-02 22:15:53 +0300

    Update randfunction3.c

--- randfunction3.c
+++ randfunction3.c
@@ -1,3 +1,15 @@
+/*
+Write a C program to calculate the dot product of 2D two vectors. Consider we have two 2D
+vectors such as A and B. Therefore, each vector has two components such as (a1, a2) and (b1,
+b2). The dot product of 2D two vectors is calculated as follows:
+𝑨. 𝑩 = ∑𝐴𝑖𝐵𝑖 = 𝑎1𝑏1 + 𝑎2𝑏2
+(for intervals 1,2)
+Prompt the boundaries of the random number interval. Generate the a1, a2, b1,and b2 and print
+the numbers. The program must include the dotProduct function that receives these components
+and returns nothing. In the function, calculate the dot product of these numbers and print the
+result*/
+/*P. J. Deitel and H. M. Deitel, “C How To Program Fifth Edition”, Prentice Hall, 2007.
+*/
 #include <stdio.h>
 void product(int a11,int a22,int b11,int b22){
 	int result;
